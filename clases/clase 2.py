55c87dab0bdf03fa48b647821b9261fbbc715cee
crisfelip <crisfelipbustamante@gmail.com>
2021-02-14 13:17:15 -0500

    clase 3

---
+++ clases/clase 2.py
@@ -0,0 +1,27 @@
+pruebav = True
+pruebaf = False
+print (pruebaf)
+print(pruebav)
+pruebav = pruebaf
+print(pruebaf)
+edad = 20
+estatura = 1.71
+peso = 65
+NOMBRE = "cristian felipe bustamante"
+# edad mayor o igual que 18 
+print ("#"*15,"mayor Edad", "#"*15)
+ismayorEdad = edad >= 18
+print (ismayorEdad)
+# estatura menor que 1.70
+print ("#"*15,"estatura promedio", "#"*15)
+ismayorestatura = estatura < 1.70
+print(ismayorestatura)
+# peso diferente de 65 
+print ("#"*15,"peso diferente de 65", "#"*15)
+ispesodiferente = peso != 65
+print (ispesodiferente)
+# el apellido esta en el nombre completo? 
+apellido = "bustamante"
+isapellido = apellido in NOMBRE 
+print ("#"*15,"apellido en nombre", "#"*15)
+print (isapellido) 
---
+++ clases/operaciones.py
@@ -0,0 +1,13 @@
+numeroA  =  87
+numeroB  =  83
+sumar  =  numeroA  +  numeroB
+print ( "el resultado de la suma es" , sumar )
+print (  "la suma dio { sumar } exitosamente" )
+restar  =  numeroA  -  numeroB
+print (  "la resta dio { restar } exitosamente" )
+multiplicar  =  numeroA  *  numeroB
+print (  "la multiplicar dio { multiplicar } exitosamente" )
+dividir  =  numeroA  /  numeroB
+print (  "la dividir dio { dividir } exitosamente" )
+exponente  =  numeroA ** numeroB
+print (  "la exponente dio { exponente } exitosamente" )
