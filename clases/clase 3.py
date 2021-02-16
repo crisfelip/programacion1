b87fc0e36838ead275b394d3ea967383f3e40b6b
crisfelip <crisfelipbustamante@gmail.com>
2021-02-16 07:30:24 -0500

    clase 3

---
+++ clases/clase 3.py
@@ -0,0 +1,11 @@
+#constantes
+PREGUNTA_NOMBRE = "como te llamas? : "
+PREGUNTA_EDAD = "cuantos años tienes? : "
+MENSAJE_SALUDO = "un gusto conocerte"
+PREGUNTA_ESTATURA ="cuanto mides? : "
+
+
+nombre = input(PREGUNTA_NOMBRE)
+edad =int (input (PREGUNTA_EDAD))
+print(edad+8)
+estatura = float (input(PREGUNTA_ESTATURA))
