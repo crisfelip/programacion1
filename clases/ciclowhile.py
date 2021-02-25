c77b1f56eeb1ede52ff8d540bcc02f5924576c4d
crisfelip <crisfelipbustamante@gmail.com>
2021-02-25 07:50:46 -0500

    juego y while

---
+++ clases/ciclowhile.py
@@ -0,0 +1,13 @@
+# entradas 
+saludo = "hola bienvenido"
+mensaje_ahorro = "llevas ahorrado..."
+pregunta_valor_computadora = "cuanto vale el computador? :"
+pregunta_cuanto_tienes = "cuanto llevas ahorrado? : "
+#entradas 
+print(saludo)
+valor = float(input(pregunta_valor_computadora))
+ahorrado = float(input(pregunta_cuanto_tienes))
+
+while (valor > ahorrado):
+    print(mensaje_ahorro,ahorrado, "te falta...",valor - ahorrado)
+    ahorrado = ahorrado + 1000 
---
+++ clases/juego1.py
@@ -0,0 +1,25 @@
+#constantes 
+MESNSAJE_SALUDO = "Bienvenido a este programa"
+PREGUNTAR_NUMERO ="en este juego esta un numero del 1-10 intentalo tres veces maximo suerte,ingresa tu numero: "
+PREGUNTA_FALLIDA = "no no no, fallaste, ingresa otro numero :"
+MENSAJE_ganaste = "Felicitaciones ganaste "
+MENSAJE_perdiste = "lo siento intentalo luego  "
+
+
+#codigo 
+numero_oculto = 7 
+vidas = 3 
+print (MESNSAJE_SALUDO)
+numeroIngresado = int(input(PREGUNTAR_NUMERO))
+if (numeroIngresado != numero_oculto):
+    vidas -=1
+while (numero_oculto != numeroIngresado and vidas > 0):
+    numeroIngresado = int(input(PREGUNTA_FALLIDA))
+    vidas -=1 
+
+if (vidas> 0 ):
+    print (MENSAJE_ganaste)
+    print (vidas)
+else:
+    print(MENSAJE_perdiste, "el numero era", numero_oculto)
+
